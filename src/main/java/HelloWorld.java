f6fe044133e647ee8732da3e58a73fa301310271
Michał Pryzwan <michalpryzwan@gmail.com>
2022-09-14 16:15:46 +0200

    cwiczenie tablica

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,20 @@
+import java.time.Year;
+
+public class HelloWorld {
+    public static void main(String[] args){
+                String myName = "Michał";
+                double myHight = 1.82;
+                int myAge = 32;
+                char myFirstLetterSurename = 'P';
+                System.out.println ("Czy jestem przystojny?");
+                boolean doI = true;
+                System.out.println (doI);
+        System.out.println ("Hello World!");
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(myFirstLetterSurename);
+        System.out.println (myHight);}}
+
+
+
+
---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,24 @@
+public class MainExtended {
+        public static void main(String[] args){
+
+            String myName = "Michał";
+            double myHight = 1.82;
+            double myWeight = 85.5;
+            int myAge = 32;
+            char myFirstLetterSurename = 'P';
+            boolean doI = true;
+            System.out.println ("Hello World!");
+            System.out.println ("Jak mam na imię?");
+            System.out.println (myName);
+            System.out.println ("Ile mam lat?");
+            System.out.println (myAge);
+            System.out.println ("Pierwsza litera mojego nazwiska?");
+            System.out.println (myFirstLetterSurename);
+            System.out.println ("Ile mam wzrostu?");
+            System.out.println (myHight);
+            System.out.println ("Ile ważę?");
+            System.out.println (myWeight);
+            System.out.println ("Czy jestem przystojny?");
+            System.out.println (doI);
+        }
+}
