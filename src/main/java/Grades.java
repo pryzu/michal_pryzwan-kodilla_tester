2635ecb4405550dd1dfdac533a841f9ddd34a40a
Michał Pryzwan <michalpryzwan@gmail.com>
2022-10-20 12:43:22 +0200

    21.10

--- src/main/java/Grades.java
+++ src/main/java/Grades.java
@@ -3,7 +3,42 @@ public class Grades {
     private int size;
 
     public Grades() {
-        this.grades = new int[10];
+        this.grades = new int[4];
         this.size = 0;
     }
+
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+    public double average(){
+        double average = 0;
+        for (int i = 0; i < this.size; i++){
+            average = average + this.grades[i];}
+        average = average / this.size;
+        return average;
+    }
+
+    public int lastGrade(){
+        int lastGrade = this.grades[this.grades.length -1];
+            return lastGrade;
+
+    }
+    public static void main(String[] args) {
+        Grades down = new Grades();
+        down.add(27);
+        down.add (1);
+        down.add(64);
+        down.add(98);
+
+        System.out.println("Średnia 4 liczb"+ down.average());
+
+        System.out.println("Ostatnia liczba:" + down.lastGrade());
+    }
+
 }
+
+
